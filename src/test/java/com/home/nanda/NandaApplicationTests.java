16ce623563608109577ebc08421e5da5dfb7c739
minsulee <alkwen0996@naver.com>
2022-11-14 17:30:36 +0900

    Initial project

---
+++ src/test/java/com/home/nanda/NandaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.nanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
