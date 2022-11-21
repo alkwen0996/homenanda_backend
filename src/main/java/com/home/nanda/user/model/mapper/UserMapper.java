c9da6ffb84aa281b234b22bf8acae44abbbb658d
minsulee <alkwen0996@naver.com>
2022-11-21 17:30:31 +0900

    refactor: 오타수정

--- src/main/java/com/home/nanda/user/model/mapper/UserMapper.java
+++ src/main/java/com/home/nanda/user/model/mapper/UserMapper.java
@@ -17,15 +17,10 @@ public interface UserMapper {
     //    void updatePassword(User user); // 비밀번호 수정
     User findUserById(String userId); // 회원정보 조회
     User loginUser(User user); // 로그인
-
     List<User> findAllUsers(); // 회원목록 조회
-
     // JWT 로그인
     User userInfo(String userId);
-
     void saveRefreshToken(Map<String, String> map);
-
     Object getRefreshToken(String userId);
-
     void deleteRefreshToken(Map<String, String> map);
 }
